5c518aebb7aa1e07497b952d857cc8efe76f71bf
tin.nguyen <tin.nguyen@team.enouvo.com>
2023-05-05 10:22:58 +0700

    fix swagger

--- src/main.ts
+++ src/main.ts
@@ -43,7 +43,19 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('docs', app, document, { swaggerUrl: '/api-json' });
+  SwaggerModule.setup('docs', app, document, {
+    customSiteTitle: 'Backend Generator',
+    customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  });
 
   await app.listen(configService.getOrThrow('app.port', { infer: true }));
 }
