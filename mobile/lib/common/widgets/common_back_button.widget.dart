7a60f86e3244139d7d91bb183fd6d183901040fc
JustQtie <qtiepiece@gmail.com>
2023-06-06 17:37:08 +0700

    [Feat] Comment fix..

--- mobile/lib/common/widgets/common_back_button.widget.dart
+++ mobile/lib/common/widgets/common_back_button.widget.dart
@@ -18,7 +18,7 @@ class CommonBackButton extends StatelessWidget {
             maxHeight: 40,
           ),
           decoration: BoxDecoration(
-            color: const Color(0xFFE5E5E5),
+            color: ColorStyles.backgroundAppbar,
             borderRadius: BorderRadius.circular(5),
           ),
           child: const Icon(
