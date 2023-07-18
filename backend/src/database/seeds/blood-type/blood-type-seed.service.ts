b6af8693b96786f69714a404574e799acfe33e22
Tran Dai Quy <trandaiquy2903@gmail.com>
2023-07-18 16:39:47 +0700

    fix(db): migration & seed

--- backend/src/database/seeds/blood-type/blood-type-seed.service.ts
+++ backend/src/database/seeds/blood-type/blood-type-seed.service.ts
@@ -17,15 +17,19 @@ export class BloodTypeSeedService {
       await this.repository.save(
         this.repository.create([
           {
+            id: 1,
             name: 'A',
           },
           {
+            id: 2,
             name: 'B',
           },
           {
+            id: 3,
             name: 'O',
           },
           {
+            id: 4,
             name: 'AB',
           },
         ]),
