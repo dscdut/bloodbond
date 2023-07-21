1a07e9aa2c097593985450b24476aae4ca915c9b
Tran Dai Quy <trandaiquy2903@gmail.com>
2023-07-21 20:52:23 +0700

    fix: build Docker local image

--- backend/script/deploy.sh
+++ backend/script/deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-# stop and remove all containers
-docker compose stop     
-docker compose rm -f
-# pull latest image from DockerHub
-docker compose pull
-# rebuilt project
-docker compose up -d
-# remove all dangling images
-docker image prune -f
+DOCKER_IMAGE="high10hunter/node"
+
+# Stop and remove the container
+docker kill bloodbond
+docker rm bloodbond
+docker rmi $DOCKER_IMAGE
+
+# Build the Docker image
+docker build -t $DOCKER_IMAGE --build-arg MACHINE_NAME=$DOCKER_IMAGE .
+docker run -p 3000:3000 --name bloodbond -d $DOCKER_IMAGE
