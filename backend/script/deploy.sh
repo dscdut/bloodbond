09430f91085d0c3d2a32c433e294fdc84199a52a
Tran Dai Quy <trandaiquy2903@gmail.com>
2023-07-23 14:55:38 +0700

    test ci/cd

--- backend/script/deploy.sh
+++ backend/script/deploy.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
-# DOCKER_IMAGE="high10hunter/node"
-
-# # Stop and remove the container
-# docker kill bloodbond
-# docker rm bloodbond
-# docker rmi $DOCKER_IMAGE
-
-# # Build the Docker image
-# docker build -t $DOCKER_IMAGE --build-arg MACHINE_NAME=$DOCKER_IMAGE .
-# docker run -p 3000:3000 --name bloodbond -d $DOCKER_IMAGE
-
+# Stop all containers
 docker compose stop     
 docker compose rm -f
+# Pull latest images 
 docker compose pull
+# Start all containers
 docker compose up -d
+# Remove unused images
 docker image prune -f
