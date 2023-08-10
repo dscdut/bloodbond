531d165272961d18a000fa2384ff075c33a21902
QuocFC <quocla@foxcode.org>
2023-08-10 10:52:13 +0700

    chore: modify some configs

--- mobile/lib/features/finish_verify/view/finish_verify.view.dart
+++ mobile/lib/features/finish_verify/view/finish_verify.view.dart
@@ -5,6 +5,7 @@ import 'package:bloodbond/common/widgets/common_app_bar.widget.dart';
 import 'package:bloodbond/common/widgets/common_rounded_button.widget.dart';
 import 'package:bloodbond/generated/assets.gen.dart';
 import 'package:bloodbond/generated/locale_keys.g.dart';
+import 'package:bloodbond/router/app_router.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -51,7 +52,13 @@ class _FinishVerifyView extends StatelessWidget {
                 width: 250,
                 height: 45,
                 child: CommonRoundedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.pushNamedAndRemoveUntil(
+                      context,
+                      AppRouter.root,
+                      (route) => false,
+                    );
+                  },
                   backgroundColor: ColorStyles.primary,
                   content: LocaleKeys.button_send.tr(),
                   textStyle: TextStyles.s17BoldText.copyWith(
