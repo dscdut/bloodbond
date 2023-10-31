f198bad01b8c72e9fd96707f52e2f8ba9cbc3fd8
Tran Dai Quy <trandaiquy2903@gmail.com>
2023-10-31 20:54:57 +0700

    fix(notification): apply notification type

--- backend/src/modules/notification-template/notification-template.service.ts
+++ backend/src/modules/notification-template/notification-template.service.ts
@@ -1,39 +1,55 @@
 import { Injectable } from '@nestjs/common';
 import { I18nService } from 'nestjs-i18n';
+import { PushNotificationType } from './types/push-notification.type';
 
 @Injectable()
 export class NotificationTemplateService {
   constructor(private readonly i18n: I18nService) {}
 
-  sendBloodDonationRequestNotification(): string {
-    return this.i18n.translate('events.BLOOD_DONATION_IN_NEED', {
-      lang: 'en',
-    });
+  sendBloodDonationRequestNotification(): PushNotificationType {
+    return {
+      title: 'Blood donation request',
+      body: this.i18n.translate('events.BLOOD_DONATION_IN_NEED', {
+        lang: 'en',
+      }),
+    };
   }
 
-  sendBloodDonationAcceptanceNotification(): string {
-    return this.i18n.translate('events.BLOOD_DONATION_ACCEPTED', {
-      lang: 'en',
-    });
+  sendBloodDonationAcceptanceNotification(): PushNotificationType {
+    return {
+      title: 'Blood donation accepted',
+      body: this.i18n.translate('events.BLOOD_DONATION_ACCEPTED', {
+        lang: 'en',
+      }),
+    };
   }
 
-  sendBloodDonationSearchingEndNotification(): string {
-    return this.i18n.translate('events.BLOOD_DONATION_SEARCHING_END', {
-      lang: 'en',
-    });
+  sendBloodDonationSearchingEndNotification(): PushNotificationType {
+    return {
+      title: 'Blood donation searching ended',
+      body: this.i18n.translate('events.BLOOD_DONATION_SEARCHING_END', {
+        lang: 'en',
+      }),
+    };
   }
 
-  sendCampaignCreatedNotification(campaignName: string): string {
-    return this.i18n.translate('events.CAMPAIGN_CREATED', {
-      lang: 'en',
-      args: { campaignName },
-    });
+  sendCampaignCreatedNotification(campaignName: string): PushNotificationType {
+    return {
+      title: 'Campaign created',
+      body: this.i18n.translate('events.CAMPAIGN_CREATED', {
+        lang: 'en',
+        args: { campaignName },
+      }),
+    };
   }
 
-  sendCampaignUpdatedNotification(campaignName: string): string {
-    return this.i18n.translate('events.CAMPAIGN_UPDATED', {
-      lang: 'en',
-      args: { campaignName },
-    });
+  sendCampaignUpdatedNotification(campaignName: string): PushNotificationType {
+    return {
+      title: 'Campaign updated',
+      body: this.i18n.translate('events.CAMPAIGN_UPDATED', {
+        lang: 'en',
+        args: { campaignName },
+      }),
+    };
   }
 }
