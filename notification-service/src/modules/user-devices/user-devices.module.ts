c0ce34366497f9938daa0f90c99cdaf77f198c4a
Tran Dai Quy <trandaiquy2903@gmail.com>
2023-11-03 10:29:37 +0700

    feat(notification): send notification to queue

--- notification-service/src/modules/user-devices/user-devices.module.ts
+++ notification-service/src/modules/user-devices/user-devices.module.ts
@@ -3,12 +3,16 @@ import { UserDevicesController } from './user-devices.controller';
 import { UserDevicesService } from './user-devices.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserDevice, UserDeviceSchema } from './user-device.schema';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: UserDevice.name, schema: UserDeviceSchema },
     ]),
+    BullModule.registerQueue({
+      name: 'userId',
+    }),
   ],
   controllers: [UserDevicesController],
   providers: [UserDevicesService],
